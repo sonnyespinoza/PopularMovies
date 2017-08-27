b31814e92f4aadec006cd013b1f017a77bc805e2
Sonny <sonny@sonnyespinoza.com>
2017-08-27 11:44:01 -0600

    Added Activity .Recycleradapter Added movie_list.xml as relativelayout with imageView grid_movie_image Added RecyclerAdpater Updated MainActivity: mMovieGrid and reference for recyclerview

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.android.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mMovieGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //reference to recyclerview for displaying movie grid
+        mMovieGrid = (RecyclerView) findViewById(R.id.rv_movies);
+
+
+
     }
 }
