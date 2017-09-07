96853804b64ab0701b94edbb0f24693009c541cf
Sonny <sonny@sonnyespinoza.com>
2017-09-06 22:02:47 -0600

    Added manifest label entry for sub activity titleadded details activty  title to string.xml

--- app/src/main/java/com/example/android/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/example/android/popularmovies/DetailsActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.popularmovies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        Intent intent = getIntent();
+
+        String msg = intent.getStringExtra("title");
+
+        TextView textView = (TextView) findViewById(R.id.tv_title);
+        textView.setText(msg);
     }
 }
