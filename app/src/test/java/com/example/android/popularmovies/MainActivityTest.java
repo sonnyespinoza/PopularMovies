34557734ce4342cce222ed58eb74f7e73862c161
Sonny <sonny@sonnyespinoza.com>
2018-01-13 12:03:00 -0700

    Added favorites menu item misc work on unit tests

---
+++ app/src/test/java/com/example/android/popularmovies/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.android.popularmovies;
+
+import org.junit.Test;
+
+/**
+ * Created by sonny on 1/13/18.
+ */
+public class MainActivityTest {
+
+    @Test
+    public void networkValidator(){
+
+    }
+
+}
